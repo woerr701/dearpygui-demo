771e7d8720fbccff4094c029ad78b69eea1afea9
woerr701 <ryanpwoerner@gmail.com>
2022-06-10 10:29:35 -0700

    Adding IDs

--- dearpygui_demo/app.py
+++ dearpygui_demo/app.py
@@ -3,14 +3,22 @@
 import dearpygui.dearpygui as dpg
 
 dpg.create_context()
+wind_id = dpg.generate_uuid()
+temp_id = dpg.generate_uuid()
+output_id = dpg.generate_uuid()
+
+def callback():
+    print("Wind ID: " + str(dpg.get_value(wind_id)))
+
 dpg.create_viewport(title='Windchill Calculator', width=600, height=300)
 
 with dpg.window(label="Windchill Calculator", width=600, height=300):
     dpg.add_text("Hello, welcome to the Windchill Calculator")
-    dpg.add_input_int(label="Wind Speed")
-    dpg.add_input_int(label="Temperature")
-    dpg.add_button(label="Enter")
+    dpg.add_input_int(label="Wind Speed",width=100, tag=wind_id)
+    dpg.add_input_int(label="Temperature",width=100, tag=temp_id)
+    dpg.add_button(label="Enter", callback=callback)
     dpg.add_button(label="Clear")
+    dpg.add_text("", tag=output_id)
     
 dpg.setup_dearpygui()
 dpg.show_viewport()
